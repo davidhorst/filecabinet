0221a7ebbbbed3d63bca4b3e0b8546ff03b0a0d1
perkinsb01 <spencer@spencerperkins.net>
2016-07-26 10:04:42 -0700

    added event and note related view def stubs and filled in the model.py

--- apps/main/models.py
+++ apps/main/models.py
@@ -1,5 +1,29 @@
-from __future__ import unicode_literals
+class Property(models.Model):
+    description = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+    user = models.ForeignKey(User)
 
-from django.db import models
+class Event(models.Model):
+    comment = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+    property = models.ForeignKey(Property)
 
-# Create your models here.
+class Note(models.Model):
+    comment = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+    event = models.ForeignKey(Event)
+
+class File(models.Model):
+    docfile = models.FileField(upload_to='documents/%Y/%m/%d')
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+    note = models.ForeignKey(Note)
+
+class Alert(models.Model):
+    when = models.DateTimeField()
+    event = models.ForeignKey(Event)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
--- apps/main/views.py
+++ apps/main/views.py
@@ -11,10 +11,48 @@ def index(request):
 def dashboard(request):
 
 	if request.user.is_authenticated():
-		return render(request, "main/event.html")
+		return render(request, "main/note.html")
 	else:
 		return redirect(reverse('accounts:login'))
 
 def properties(request):
 
 	return render (request, "main/properties.html")
+
+def events(request):
+	#ajax api for events?
+	# returns a list of all of the events?
+	pass
+
+def event(request,id):
+	# db call to get out specific event to display rendered html to user
+	#server side rendering
+	pass
+
+def add_event(request):
+	#creates a new event. will need to return a server rendered html
+	pass
+
+
+def add_note(request):
+    # example from fileupload project
+    if request.method == 'POST':
+        form = DocumentForm(request.POST, request.FILES)
+        if form.is_valid():
+            newdoc = Document(docfile=request.FILES['docfile'])
+            newdoc.save()
+
+            # Redirect to the document list after POST
+            return HttpResponseRedirect(reverse('list'))
+    else:
+        form = DocumentForm()  # A empty, unbound form
+
+    # Load documents for the list page
+    documents = Document.objects.all()
+
+    # Render list page with the documents and the form
+    return render(
+        request,
+        'list.html',
+        {'documents': documents, 'form': form}
+    )
