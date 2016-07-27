ff694806a99817f3b4178e755b39adba1b9e3bbe
Jun Kyu Yi <junkyuyi@gmail.com>
2016-07-27 13:41:00 -0700

    added additional column to model:alert

--- apps/main/models.py
+++ apps/main/models.py
@@ -51,6 +51,7 @@ class File(models.Model):
 
 class Alert(models.Model):
     when = models.DateTimeField()
+    comment = models.CharField(max_length=50)
     event = models.ForeignKey(Event)
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
